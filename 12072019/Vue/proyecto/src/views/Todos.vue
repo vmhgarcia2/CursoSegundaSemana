e0775aac868b547af373855c7c8af1c35d2111a0
vmhgarcia2 <52398634+vmhgarcia2@users.noreply.github.com>
2019-07-12 10:32:39 +0200

    Incrementar

---
+++ 12072019/Vue/proyecto/src/views/Todos.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{nombre}}</h1>
+    <h3>{{numero}}</h3>
+    <button v-on:click="incrementar">Incrementar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      nombre: 'mundo',
+      numero: 0,
+    };
+  },
+  methods: {
+    incrementar() {
+      this.numero = this.numero + 1;
+    },
+  },
+};
+</script>
+
+<style>
+
+</style>

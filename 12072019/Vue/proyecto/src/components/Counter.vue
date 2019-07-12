96542dc9b3765eb80b372919432c05a72be840f5
vmhgarcia2 <52398634+vmhgarcia2@users.noreply.github.com>
2019-07-12 12:24:44 +0200

    Counter

---
+++ 12072019/Vue/proyecto/src/components/Counter.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <h1>{{nombre}}</h1>
+    <h3>{{numero}}</h3>
+    <input v-model="numero"/>
+    <button v-on:click="incrementar" v-bind:disabled="IncrementIsDisabled">Incrementar</button>
+    <button v-on:click="decrementar" v-bind:disabled="DecrementIsDisabled">Decrementar</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      numero: 0,
+    };
+  },
+  props: {
+    min: {
+      type: Number,
+      default: 0,
+    },
+    max: {
+      type: Number,
+      default: 10,
+    },
+    initValue: {
+      type: Number,
+      default: 10,
+    },
+  },
+  methods: {
+    incrementar() {
+      this.numero += 1;
+    },
+    decrementar() {
+      this.numero -= 1;
+    },
+  },
+  watch: {
+    numero(val, oldvalue) {
+      console.log(val, oldvalue);
+    },
+  },
+  computed: {
+    IncrementIsDisabled() {
+      return this.numero >= this.max;
+    },
+    DecrementIsDisabled() {
+      return this.numero <= this.min;
+    },
+  },
+  created() {
+    this.numero = this.initValue;
+    console.log('He sido creado', this.initValue);
+  },
+};
+</script>
--- 12072019/Vue/proyecto/src/views/Todos.vue
+++ 12072019/Vue/proyecto/src/views/Todos.vue
@@ -1,27 +1,26 @@
 <template>
   <div>
-    <h1>{{nombre}}</h1>
-    <h3>{{numero}}</h3>
-    <button v-on:click="incrementar">Incrementar</button>
+    <h1>Hay algo</h1>
+    <Counter :initValue = "200"/>
+    <Counter :initValue = "3"/>
+    <Counter/>
+    <Counter/>
+    <Counter/>
   </div>
 </template>
 
 <script>
+
+import Counter from '../components/Counter.vue';
+
 export default {
+  components: {
+    Counter,
+  },
   data() {
     return {
       nombre: 'mundo',
-      numero: 0,
     };
-  },
-  methods: {
-    incrementar() {
-      this.numero = this.numero + 1;
-    },
   },
 };
 </script>
-
-<style>
-
-</style>
